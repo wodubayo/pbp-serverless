2340aac99d507cf2692043c5c9d9f86f2f2f05e6
Waheed Odubayo <wodubayo@gmail.com>
2018-05-02 16:33:56 -0500

    corrected spelling errors

--- GetHeroes/index.js
+++ GetHeroes/index.js
@@ -14,7 +14,7 @@ module.exports = function(context, req) {
         .find()
         .toArray((err, result) => {
           if (err) throw err;
-          context.log("Result retieved successfully. Beauty!");
+          context.log("Result retrieved successfully. Beauty!");
           result.forEach(hero => delete hero._id);
           context.res = {
             status: 200,
